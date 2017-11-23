7487e686ef1709b9daa2505c0d58b4ca002ac71e
Paul Woods <paul@woodscreativemedia.com>
2017-11-23 09:34:12 +0000

    Updates to data format

---
+++ php/days.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * days
+ * @see https://github.com/woodscreative/data_lib
+ */
+$data = array (
+  '01' => 'Monday',
+  '02' => 'Tuesday',
+  '03' => 'Wednesday',
+  '04' => 'Thursday',
+  '05' => 'Friday',
+  '06' => 'Saturday',
+  '07' => 'Sunday',
+);
+?>
